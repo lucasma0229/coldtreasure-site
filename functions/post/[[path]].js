21c4f11781b24d2602ce39280620f61fc4986e86
lucasma0229 <lucasma0229@gmail.com>
2026-03-02 15:39:29 +0800

    Delete functions/post directory

--- functions/post/[[path]].js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-
-  // /post 或 /post/<slug> 都改写到 /post/index.html
-  url.pathname = "/post/index.html";
-
-  return fetch(new Request(url.toString(), context.request));
-}
